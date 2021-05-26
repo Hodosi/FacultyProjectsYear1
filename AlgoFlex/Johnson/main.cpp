4eb2deb7aca999aa8f5bfc943224e72f10de9ca2
Attila <hodosi.attila@yahoo.com>
2021-05-26 20:33:30 +0300

    Have a nice day

--- AlgoFlex/Johnson/main.cpp
+++ AlgoFlex/Johnson/main.cpp
@@ -1,114 +1,258 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <string>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-ifstream fin("D:\\FacultyProjects\\GraphAlgorithms\\DFS\\in.txt");
+ifstream  fin("D:\\FacultyProjects\\AlgoFlex\\Johnson\\in.txt");
+ofstream fout("D:\\FacultyProjects\\AlgoFlex\\Johnson\\out.txt");
 
 typedef struct{
+
     int id;
-    string color;
-    int time_start;
-    int time_end;
+    int distance;
     int parent;
+    bool visited;
 } Node;
 
-vector<vector<Node*>> adjacency_list;
+typedef struct {
+
+    int weight;
+    Node* predecessor;
+    Node* successor;
+} Edge;
+
+int nr_nodes, nr_edges;
 vector<Node*> node_list;
-int nr_nodes;
+vector<Edge*> edge_list;
+vector<vector<pair<Node*, int>>> adjacency_list;
+vector<vector<int>> distance_matrix;
 
-void fromFileToAdjacencyList(){
-    int predecessor_id, successor_id, cost;
-    while (fin >> predecessor_id >> successor_id >> cost){
+void fromFileToEdgeList(){
+
+    int predecessor_id, successor_id, weight;
+    while (fin >> predecessor_id >> successor_id >> weight){
+        Edge *edge = new Edge();
+
         Node *predecessor, *successor;
-        predecessor = node_list[predecessor_id];
-        successor = node_list[successor_id];
+        predecessor = node_list[predecessor_id + 1];
+        successor = node_list[successor_id + 1];
 
-        adjacency_list[predecessor_id].push_back(successor);
-        adjacency_list[successor_id].push_back(predecessor);
+        edge -> predecessor = predecessor;
+        edge -> successor = successor;
+        edge -> weight = weight;
+
+        edge_list.push_back(edge);
+    }
+}
+
+void printEdgeList(){
+
+    for(auto edge : edge_list){
+        cout << edge -> predecessor -> id << " " << edge -> successor -> id << " " << edge -> weight << "\n";
+    }
+}
+
+void fromEdgeListToAdjacencyList(){
+
+    for(auto edge : edge_list){
+        Node *successor = edge->successor;
+        Node *predecessor = edge -> predecessor;
+        adjacency_list[predecessor -> id].push_back(make_pair(successor, edge->weight));
     }
 }
 
 void printAdjacencyList(){
+
+    cout << "\n";
     int predecessor;
-    for(predecessor = 1; predecessor <= nr_nodes; predecessor++){
+    for(predecessor = 0; predecessor <= nr_nodes; predecessor++){
         cout << predecessor << " : ";
         for(auto successor : adjacency_list[predecessor]){
-            cout << successor -> id << " ";
+            cout << successor.first -> id << " , " << successor.second << " ; ";
         }
         cout << "\n";
     }
     cout << "\n";
 }
 
-int dfsVisit(Node *current_node, int time){
-    time++;
-    current_node -> time_start = time;
-    current_node -> color = "grey";
+void initialization(Node* source_node){
 
-    for(auto successor : adjacency_list[current_node -> id]){
-        if(successor -> color == "white"){
-            successor -> parent = current_node -> id;
-            time = dfsVisit(successor, time);
-        }
+    for(auto node : node_list){
+        node -> distance = INT16_MAX;
+        node -> parent = -1;
+        node->visited = false;
     }
-
-    current_node -> color = "black";
-
-    time++;
-    current_node  -> time_end = time;
-
-    cout << current_node -> id << " ";
-    cout << current_node -> time_start << " " << current_node -> time_end << "\n";
-
-    return time;
+    source_node -> distance = 0;
 }
 
-void recursivelyDfs(){
-    int time;
-
-    for(int i = 1; i <= nr_nodes; i++) {
-        auto node = node_list[i];
-        node -> color = "white";
-        node -> time_start = -1;
-        node -> time_end = -1;
-        node -> parent = -1;
+bool relax(Node* predecessor, Node* successor, int weight){
+    if(successor -> distance > predecessor -> distance + weight){
+        successor -> distance = predecessor -> distance + weight;
+        successor -> parent = predecessor -> id;
+        return true;
     }
+    return false;
+}
 
-    time = 0;
+bool bellmanFord(Node *source_node){
 
-    for(int i = 1; i <= nr_nodes; i++) {
-        auto node = node_list[i];
-        if (node -> color == "white") {
-            time = dfsVisit(node, time);
+    initialization(source_node);
+    for(int i = 1; i < node_list.size(); i++){
+        for(auto edge : edge_list){
+            relax(edge -> predecessor, edge -> successor, edge -> weight);
         }
     }
-    cout << "\n";
+
+    for(auto edge : edge_list){
+        if(relax(edge -> predecessor, edge -> successor, edge -> weight)){
+            return false;
+        }
+    }
+    return true;
+}
+
+struct costumeCmp{
+
+    bool operator()(const Node* predecessor, const Node* successor){
+        return  (predecessor -> distance > successor -> distance);
+    }
+};
+
+void dijkstra(Node *source_node){
+
+    initialization(source_node);
+
+    priority_queue<Node*, vector<Node*>, costumeCmp > p_queue;
+    p_queue.push(source_node);
+
+    while (!p_queue.empty()){
+        auto predecessor = p_queue.top();
+        p_queue.pop();
+        if(predecessor->visited){
+            continue;
+        }
+        for(auto successor : adjacency_list[predecessor -> id]){
+            if(relax(predecessor, successor.first, successor.second)){
+                p_queue.push(successor.first);
+            }
+        }
+        predecessor->visited = true;
+    }
+}
+
+bool johnson(){
+
+    // add edges from new node (node 0) to all other nodes and mark its weight with 0
+    for(int i = 1; i < node_list.size(); i++){
+        Edge *edge = new Edge();
+
+        Node *predecessor, *successor;
+        predecessor = node_list[0];
+        successor = node_list[i];
+
+        edge -> predecessor = predecessor;
+        edge -> successor = successor;
+        edge -> weight = 0;
+
+        edge_list.push_back(edge);
+    }
+
+
+    if (!bellmanFord(node_list[0])){
+        return false;
+    }
+
+    // distance list from new node to others
+    vector<int> distance_from_new_node(nr_nodes);
+    for(auto node : node_list){
+        distance_from_new_node[node->id] = node->distance;
+    }
+
+    // reweighting
+    for(auto edge : edge_list){
+        int predecessor_id = edge->predecessor->id;
+        int successor_id = edge->successor->id;
+        edge->weight = edge->weight + distance_from_new_node[predecessor_id] - distance_from_new_node[successor_id];
+    }
+
+    fromEdgeListToAdjacencyList();
+    printAdjacencyList();
+
+    // create distance matrix
+    for(auto source_node : node_list){
+        dijkstra(source_node);
+        for(auto destination_node : node_list){
+            int source_id = source_node->id;
+            int destination_id = destination_node->id;
+            int min_distance_from_source = destination_node -> distance;
+            if(min_distance_from_source != INT16_MAX){
+                distance_matrix[source_id][destination_id] = min_distance_from_source - distance_from_new_node[source_id] + distance_from_new_node[destination_id];
+            }
+            else{
+                distance_matrix[source_id][destination_id] = min_distance_from_source;
+            }
+        }
+    }
+
+    return true;
+}
+
+void printMinPathMatrix(){
+
+    for(auto edge : edge_list){
+        if(edge->predecessor->id != 0){
+            fout << edge -> predecessor -> id - 1 << " ";
+            fout << edge -> successor -> id - 1 << " ";
+            fout << edge -> weight << "\n";
+        }
+    }
+
+    for(int i = 1; i < distance_matrix.size(); i++){
+        for(int j = 1; j < distance_matrix[0].size(); j++){
+            if(distance_matrix[i][j] == INT16_MAX){
+                fout << "INF" << " ";
+            }
+            else{
+                fout << distance_matrix[i][j] << " ";
+            }
+        }
+        fout << "\n";
+    }
 }
 
 int main() {
-    fin >> nr_nodes;
+    fin >> nr_nodes >> nr_edges;
 
     node_list.resize(nr_nodes + 1, NULL);
-
-    for(int id = 1; id <= nr_nodes; id++){
+    for(int id = 0; id <= nr_nodes; id++){
         Node *new_node = new Node();
         new_node -> id = id;
         node_list[id] = new_node;
     }
 
+    fromFileToEdgeList();
+    printEdgeList();
+
     adjacency_list.resize(nr_nodes + 1);
 
-    fromFileToAdjacencyList();
-    printAdjacencyList();
-
-    recursivelyDfs();
+    distance_matrix.resize(nr_nodes + 1, vector<int>(nr_nodes + 1));
+    bool valid = johnson();
+    if(valid){
+        printMinPathMatrix();
+    }
+    else{
+        fout << -1;
+    }
 
     for(auto node : node_list){
         delete node;
     }
+
+    fin.close();
+    fout.close();
 
     return 0;
 }
